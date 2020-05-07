ecd19b70b35916db52bb943c6f7881a1de416c24
xianglapangpanghu <2994014137@qq.com>
2020-05-07 10:03:16 +0800

    1

---
+++ 爬虫/1/a.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+import bs4
+
+def getHTMLTEXT(url):#获取网页
+    try:
+        r = requests.get(url)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return " "
+
+def fillUnivList(ulist, html):#解析网页
+    soup = BeautifulSoup(html, "html.parser")
+    for tr in soup.find('tbody').children:
+        if isinstance(tr, bs4.element.Tag):
+            tds = tr('td')
+            ulist.append([tds[0].string, tds [1].string, tds[2].string, tds[3].string])
+
+def printUnivList(ulist, num):#输出内容
+    tplt = "{0:^10}\t{1:{4}^6}\t{2:^20}\t{2:^10}"
+    print(tplt.format("排名","学校名称","省份","总分",chr(12288)))
+    for i in range(num):
+        u = ulist[i]
+        print(tplt.format(u[0], u[1], u[2], u[3],chr(12288)))
+    print("Suc" + str(num))
+
+if __name__ == "__main__":
+    uinfo = []
+    url = "http://www.zuihaodaxue.com/zuihaodaxuepaiming2019.html"
+    html = getHTMLTEXT(url)
+    fillUnivList(uinfo, html)
+    printUnivList(uinfo, 10)
---
+++ 爬虫/豆瓣top250/top250.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+import random
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0",
+    "Connection": "keep-alive"
+}
+movie_list = []
+def get_pages_link():
+    # https://movie.douban.com/top250?start=25
+    for item in range(0,250,25):
+        url = "https://movie.douban.com/top250?start={}".format(item)
+        web_data = requests.get(url,headers=headers)
+        time.sleep(1 + random.random())
+        soup = BeautifulSoup(web_data.text,'lxml')
+        for movie in soup.select('#wrapper li'):
+            href = movie.select('.hd > a')[0]['href']
+            name = movie.select('.hd > a > span')[0].text
+            star = movie.select('.rating_num')[0].text
+            people = movie.select('.star > span')[3].text
+            try:
+                quote = movie.select('.inq')[0].text
+            except :
+                print("没有quote哦")
+                quote = None
+            data = {
+                'url':href,
+                '评价人数':people,
+                '片名':name,
+                '评分':star,
+                '名言':quote
+            }
+            print(data)
+        print('\n'+' - '*50+'\n')
+
+if __name__ =='__main__':
+    get_pages_link()
